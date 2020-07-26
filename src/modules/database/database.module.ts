9b35b8eda457d860f26e7384dc7baf870e848803
Anabel <anabel.andrescosta9601@gmail.com>
2020-07-25 21:06:01 -0700

    25/07/2020-Add new features in database

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -30,7 +30,7 @@ import * as fs from 'fs'
         TypeOrmModule.forRoot({
             type: 'mysql',
             host: '127.0.0.1',
-            port: 3308,
+            port: 3306,
             username: 'root',
             password: '',
             database: 'cubamuseo',
